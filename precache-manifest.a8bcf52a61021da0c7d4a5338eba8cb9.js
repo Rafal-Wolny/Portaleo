bb461688b31e3264e5c3669572f53d3b156ae053
Rafal-Wolny <rafwo96@gmail.com>
2020-07-20 18:54:25 +0200

    Updates

---
+++ precache-manifest.a8bcf52a61021da0c7d4a5338eba8cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eb3cfd88bdc80c4c94abafa5174102",
+    "url": "/Portaleo/index.html"
+  },
+  {
+    "revision": "131a97d1cc00e2c79867",
+    "url": "/Portaleo/static/css/2.6b00acf2.chunk.css"
+  },
+  {
+    "revision": "77dffa9b07ce87028115",
+    "url": "/Portaleo/static/css/main.413b2518.chunk.css"
+  },
+  {
+    "revision": "131a97d1cc00e2c79867",
+    "url": "/Portaleo/static/js/2.4176c926.chunk.js"
+  },
+  {
+    "revision": "77dffa9b07ce87028115",
+    "url": "/Portaleo/static/js/main.cd4e7cb6.chunk.js"
+  },
+  {
+    "revision": "27c86193a3d68b152f55",
+    "url": "/Portaleo/static/js/runtime-main.43c1b384.js"
+  },
+  {
+    "revision": "807e2b89d7332af548336e8e8ef83251",
+    "url": "/Portaleo/static/media/background.807e2b89.jpg"
+  }
+]);
